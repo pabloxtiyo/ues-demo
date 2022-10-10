04741ae4edb592af17afe1084334a2a24f616e78
Alan Alvarenga <alan.alvarengamejia@telus.com>
2022-10-09 21:39:32 -0600

    proxy for simple integration

--- libs/reminder/src/lib/reminder.tsx
+++ libs/reminder/src/lib/reminder.tsx
@@ -1,24 +1,47 @@
-
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Add from './add/add';
 import Display from './display/display';
 import styles from './reminder.module.css';
-import {STATES, TraceableTask} from "../index";
+import { PersonalTask, STATES, TraceableTask } from '../index';
+import axios from 'axios';
 
 /* eslint-disable-next-line */
 export interface ReminderProps {}
 
 export function Reminder(props: ReminderProps) {
   const [tasks, setTasks] = useState(Array<TraceableTask>);
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await axios.get('/api/personal-task');
+      setTasks(data.map((e: TraceableTask) => new PersonalTask(e.task)));
+    };
+    fetch();
+  }, []);
   const appendTask = (task: TraceableTask) => {
-    setTasks([...tasks, task]);
-  }
+    const fetch = async () => {
+      const { data } = await axios.post('/api/personal-task', task);
+      setTasks([...tasks, data]);
+    };
+    fetch();
+  };
   return (
-    <div className={styles['container']} data-testid='container'>
-      <div><Add append={appendTask}/></div>
-      <div><Display key={STATES.PENDING} items={tasks} state={STATES.PENDING} /></div>
-      <div><Display key={STATES.IN_PROGRESS} items={tasks} state={STATES.IN_PROGRESS} /></div>
-      <div><Display key={STATES.DONE} items={tasks} state={STATES.DONE} /></div>
+    <div className={styles['container']} data-testid="container">
+      <div>
+        <Add append={appendTask} />
+      </div>
+      <div>
+        <Display key={STATES.PENDING} items={tasks} state={STATES.PENDING} />
+      </div>
+      <div>
+        <Display
+          key={STATES.IN_PROGRESS}
+          items={tasks}
+          state={STATES.IN_PROGRESS}
+        />
+      </div>
+      <div>
+        <Display key={STATES.DONE} items={tasks} state={STATES.DONE} />
+      </div>
     </div>
   );
 }
